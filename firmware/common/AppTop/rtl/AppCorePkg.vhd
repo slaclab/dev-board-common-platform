82406eb9e8ac53d775f6d9b5547432a25b2e0629
till straumann <till.straumann@alumni.tu-berlin.de>
2018-06-22 15:58:09 -0700

    Changed project name in license headers

--- firmware/common/AppTop/rtl/AppCorePkg.vhd
+++ firmware/common/AppTop/rtl/AppCorePkg.vhd
@@ -4,11 +4,11 @@
 -------------------------------------------------------------------------------
 --Description: 
 -------------------------------------------------------------------------------
---This file is part of 'Example Project Firmware'.
+--This file is part of 'DevBoard Common Platform'.
 --It is subject to the license terms in the LICENSE.txt file found in the 
 --top-level directory of this distribution and at: 
 --https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
---No part of 'Example Project Firmware', including this file, 
+--No part of 'DevBoard Common Platform', including this file, 
 --may be copied, modified, propagated, or distributed except according to 
 --the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
