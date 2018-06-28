96aebc667565aed6edb6c5764e36e3130cf6ca3b
Till Straumann <strauman@slac.stanford.edu>
2018-06-28 14:36:28 -0700

    Use SystemManagement '.xci' instead of '.dcp'
    Vivado produces a critical warning; apparently the use of
    OOC dcps is discouraged.

--- firmware/common/UltraScale/ruckus.tcl
+++ firmware/common/UltraScale/ruckus.tcl
@@ -3,5 +3,5 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/rtl/"
-loadSource -path "$::DIR_PATH/ip/SystemManagementCore.dcp"
-#loadIpCore  -path "$::DIR_PATH/ip/SystemManagementCore.xci"
+#loadSource -path "$::DIR_PATH/ip/SystemManagementCore.dcp"
+loadIpCore  -path "$::DIR_PATH/ip/SystemManagementCore.xci"
