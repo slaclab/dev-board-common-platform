885b0ff150ec5dd94cf190a093bdfdc7710fd67c
Till Straumann <strauman@slac.stanford.edu>
2018-06-28 23:05:37 -0700

    Added comment about avoiding OOC DCPs

--- firmware/common/UltraScale/ruckus.tcl
+++ firmware/common/UltraScale/ruckus.tcl
@@ -3,5 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/rtl/"
+# Recent (2018.1) vivado warns that loading OOC DCPs is not
+# recommended and .xci should be used...
+#
 #loadSource -path "$::DIR_PATH/ip/SystemManagementCore.dcp"
 loadIpCore  -path "$::DIR_PATH/ip/SystemManagementCore.xci"
